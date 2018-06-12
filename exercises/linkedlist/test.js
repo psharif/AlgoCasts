89dab9e9c92d713f52ec353fac873ed427de151c
psharif <psharif2788@gmail.com>
2018-06-11 19:51:16 -0700

    Added linkedList functions

--- exercises/linkedlist/test.js
+++ exercises/linkedlist/test.js
@@ -218,7 +218,7 @@ describe('RemoveAt', () => {
   });
 });
 
-describe.skip('InsertAt', () => {
+describe('InsertAt', () => {
   test('inserts a new node with data at the 0 index when the list is empty', () => {
     const l = new List();
     l.insertAt('hi', 0);
@@ -273,7 +273,7 @@ describe.skip('InsertAt', () => {
   });
 });
 
-describe.skip('ForEach', () => {
+describe('ForEach', () => {
   test('applies a transform to each node', () => {
     const l = new List();
 
@@ -293,7 +293,7 @@ describe.skip('ForEach', () => {
   });
 });
 
-describe.skip('for...of loops', () => {
+describe('for...of loops', () => {
   test('works with the linked list', () => {
     const l = new List();
 
